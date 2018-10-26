b7701db7b717db775aa3a34c3d0b0f73236578a2
Anthony Oteri <anthony.oteri@edgeware.tv>
2018-10-26 13:38:32 -0400

    Allow all hosts

--- timetrack/settings.py
+++ timetrack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "g&l44#ua72xj40k5e5ef+p+@!5ana%4&5#=uhp6@s13a55+t22"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
